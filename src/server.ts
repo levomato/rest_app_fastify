e5bcf218f3a93845529a0637796cc9e4a578126a
Levin Kerschberger <levinkerschberger@novatec-gmbh.de>
2023-01-29 11:38:29 +0100

    Schema und Routen im Server regisitriert

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import Fastify from "fastify";
+import postRoutes from "./post/post.routes";
+import { postSchemas } from "./post/post.schema";
 
 function buildServer() {
     const server = Fastify();
+
+    for (const schema of [...postSchemas]) {
+        server.addSchema(schema);
+    }
+
+    server.register(postRoutes, { prefix: "api/posts" })
+
     return server
 }
 
